6f6bc700307611651e0c14f0ae52e1adcd7358cc
Nijaz Andelić <andelicnijaz@gmail.com>
2022-06-11 12:01:35 +0200

    Senbonzakura food (Version 1.0.1)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import { useState } from "react";
+import React, { Suspense } from "react";
 import { Routes, Route, Navigate, useParams } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
-import Home from "./components/Home/Home";
-import Menu from "./components/Menu/Menu";
-import MenuContent from "./components/Menu/MenuContent";
-import { useSelector } from "react-redux";
-import Orders from "./components/Orders/Orders";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+// import Home from "./components/Home/Home";
+// import Menu from "./components/Menu/Menu";
+// import MenuContent from "./components/Menu/MenuContent";
+// import Orders from "./components/Orders/Orders";
+
+const Home = React.lazy(() => import("./components/Home/Home"));
+const Menu = React.lazy(() => import("./components/Menu/Menu"));
+const MenuContent = React.lazy(() => import("./components/Menu/MenuContent"));
+const Orders = React.lazy(() => import("./components/Orders/Orders"));
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/menu" element={<Menu />}>
-          <Route
-            path="/menu/:foodId"
-            element={<MenuContent></MenuContent>}
-          ></Route>
-        </Route>
-        <Route path="/orders" element={<Orders />}></Route>
-        <Route path="/login" element={<Navigate to="/home" />}></Route>
-      </Routes>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/menu" element={<Menu />}>
+            <Route
+              path="/menu/:foodId"
+              element={<MenuContent></MenuContent>}
+            ></Route>
+          </Route>
+          <Route path="/orders" element={<Orders />}></Route>
+          <Route path="/login" element={<Navigate to="/home" />}></Route>
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
